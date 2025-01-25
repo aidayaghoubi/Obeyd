9852cc946cbedbf6368b876db01ea0217444f071
aydaYaghoubi <idayaghoubi9@gmail.com>
2025-01-25 23:56:04 +0330

    fix add joke bug

--- app/jokes/page.jsx
+++ app/jokes/page.jsx
@@ -3,16 +3,22 @@ import Layout from '../components/Layout';
 import JokeDirectory from './JokeDirectory';
 import AddJoke from '../components/AddJoke/AddJoke';
 import { useJokes } from '@/context/JokeContext';
+import { useAuth } from '@/context/AuthContext';
 
 
 export default function JokesPage() {
 
   const { jokes } = useJokes()
+  const { user } = useAuth()
+
 
   return (
     <Layout>
       <JokeDirectory jokes={jokes} />
-      <AddJoke />
+      {user?.name &&
+
+        <AddJoke />
+      }
     </Layout>
   );
 }
